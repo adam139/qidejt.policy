cc82306513e4b585fcdddc75a736b1dd72a1f3cd
adam139 <yuejun.tang@gmail.com>
2018-06-15 18:03:01 +0800

    add compiled resource files

---
+++ qidejt/policy/browser/static/homepage-compiled.js
@@ -0,0 +1,30 @@
+
+require([
+  'jquery','bootstrap-carousel','bootstrap-tabs'
+], function($,carousel,tabs) {
+  'use strict';
+$(document).ready(function(){ 
+	$(".nav-tabs a").mouseover(function (e) {
+		  e.preventDefault();
+		  $(this).tab('show');
+		});
+	$(".nav-tabs").on("click","a",function (e) {
+		  e.preventDefault();
+		  var url = $(this).attr("data-js-target");
+		  window.location.href = url;
+		  return false;
+		});
+    $('.carousel').carousel();
+	var existnav = $('.portletNavigationTree');
+	if (existnav !== undefined) {            
+		var leftHeight = $('.portletNavigationTree dd').height();
+		var rightHeight = $('#content').parent().height();
+		if((leftHeight) && leftHeight > rightHeight) {
+			leftHeight = rightHeight;
+			$('.portletNavigationTree dd').height(leftHeight).css("overflow","auto");
+		}
+	}        
+	});
+});
+define("/home/plone/workspace/Plone5sites/sites/src/qidejt.policy/qidejt/policy/browser/static/homepage-custom.js", function(){});
+
